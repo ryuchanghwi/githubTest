aa63c9ba51c832b6a9db6033bb0d6a352384147b
ckdgnl10@naver.com <ckdgnl10@naver.com>
2021-10-19 18:20:32 +0900

    프린트 또 추가

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("hi")
         print("hello")
+        
+        print("dslf")
+        print("sdlkfjsldf")
+        
     }
 
 
